90644543a98303f107db2d12d6b27a4be49d04af
robinweide <robinweide@gmail.com>
2014-05-06 16:30:15 +0200

    com

--- CompaReads_Loop.pl
+++ CompaReads_Loop.pl
@@ -21,15 +21,13 @@ foreach my $QUERY (@QUERY){
         # print $values[0]." has begun\n";
         #print "cd ".$values[3]."\n";
         # chdir($values[0]);
-        `awk \'BEGIN\{P\=1\}\{if\(P\=\=1\|\|P\=\=2\)\{gsub\(\/\^\[\@\]\/\,\"\>\"\)\;print\}\; if\(P\=\=4\)P\=0\; \P\+\+\}\' $values[1] \> $values[0].fa`;
-      				foreach my $hoi (@QUERY){
-      				chomp $hoi;
-      				my @valus = split(/\t/,$hoi);
-        			chomp $valus[0];
-        			if ($valus[0] == $values[0]){next};
-					`awk \'BEGIN\{P\=1\}\{if\(P\=\=1\|\|P\=\=2\)\{gsub\(\/\^\[\@\]\/\,\"\>\"\)\;print\}\; if\(P\=\=4\)P\=0\; \P\+\+\}\' $valus[1] \> $valus[0].fa`;
-					`\/home\/robin\/bin\/compareads\-2\.0\.2\/compare\_reads \-a $values[0].fa -b $valus[0].fa -k 39 -t 2`;
-					 }
+      	foreach my $hoi (@QUERY){
+      		chomp $hoi;
+      		my @valus = split(/\t/,$hoi);
+        	chomp $valus[0];
+        	if ($valus[0] eq $values[0]){next};
+			`\/home\/robin\/bin\/compareads\-2\.0\.2\/compare\_reads \-a $values[0].fa -b $valus[0].fa -k 39 -t 2`;
+		}
 
         
 
