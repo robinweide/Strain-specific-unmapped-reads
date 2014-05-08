2119158a9f9d265de442b0c71088f91a7e18a516
robinweide <robinweide@gmail.com>
2014-05-08 10:07:34 +0200

    some sed-issues

--- triple_distilled.pl
+++ triple_distilled.pl
@@ -25,10 +25,10 @@ foreach my $QUERY (@QUERY){
 # CELERA-mapping
        # get fastq of all reads
        `bedtools bamtofastq \-i $values[0] \-fq all\.fastq`; 
-       `sed  \'\/\^@$values\[1\]\/ s\/\[\:\/\]\[12\]\$\/\/\' all\.fastq \> temp\.fastq`; 
+       `sed  \'\/\^\@$values[1]\/ s\/\[\:\/\]\[12\]\$\/\/\' all\.fastq \> temp\.fastq`; 
        `mv temp\.fastq all\.fastq`; 
       # get headers frmo the remaining unmapped reads
-       `sed  \'\/\^\@$values[1]\/ s\/\[\:\/\]\[12\]\$\/\/\' \| grep \-o \"\@@$values[1]\[\^\ \  \]\*\" \| sort \| uniq \| sed \'s\/\^\@\/\/\' \| sed \'s\/\[\:\/\]\[12\]\$\/\/\' \> name\_unmapped\.lst`;
+       `sed  \'\/\^\@$values[1]\/ s\/\[\:\/\]\[12\]\$\/\/\' \| grep \-o \"\@$values[1]\[\^\ \  \]\*\" \| sort \| uniq \| sed \'s\/\^\@\/\/\' \| sed \'s\/\[\:\/\]\[12\]\$\/\/\' \> name\_unmapped\.lst`;
        # get interleaved fastq of unmapped reads
        `seqtk subseq all\.fastq name\_unmapped\.lst \> ready\_for\_celera\_mapping\.fastq`;
         # map to celera in paired-end mode
