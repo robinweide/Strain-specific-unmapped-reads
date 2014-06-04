b145c9e61267c863a69e5ff7976244484593054f
robinweide <robinweide@gmail.com>
2014-06-04 18:56:50 +0200

    DoubleCask (for celera+Ychr bio/tech)

---
+++ DoubleCask.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use File::Basename;
+# print("input\-file \t shellscript_to_use");
+open(INFILE, "$ARGV[0]");
+
+my @QUERY = <INFILE>;
+close INFILE;
+
+# open(TOOL, "$ARGV[1]");
+# my $TOOL = <TOOL>;
+# close TOOL;
+my $all = '';
+
+foreach my $QUERY (@QUERY){
+        chomp $QUERY;
+        my $Qname = basename($QUERY);
+        my @values = split(/\t/,$QUERY);
+        chomp $values[3];
+        print $values[3]." has begun\n";
+        #print "cd ".$values[3]."\n";
+        `mkdir $values[3]`;
+        chdir($values[3]);
+
+` sed  \'\/\^\@$values[1]\/ s\/\[\:\/\]\[12\]\$\/\/\' $values[3]\_Unmapped\_1\.fastq \> test1\.fq`;
+`seqtk subseq test1\.fq nameMc\.lst \> celera\_m1\.fq`;
+` sed  \'\/\^\@$values[1]\/ s\/\[\:\/\]\[12\]\$\/\/\' $values[3]\_Unmapped\_2\.fastq \> test2\.fq`;
+`seqtk subseq test2\.fq nameMc\.lst \> celera\_m2\.fq`;
+
+        
+# map to celera in paired-end mode
+`\/home\/robin\/bin\/bwa\-0\.7\.5a\/bwa mem \-M \-p \/data\_fedor12\/robin\/databases\/Celera\/Alt\_Rn\_Celera\.fa celera\_m1\.fq celera\_m2\.fq \> CeleraR\.sam`;
+# get reads, that properly map in pairs
+`samtools view \-bS \-f 2 CeleraR\.sam \> Celera\_proper\_mappedR\.bam`;
+
+`bedtools intersect \-abam Celera\_proper\_mappedR\.bam \-wa \-u -b celeraRegiosDiegemaptZijnDoorEve.bed) \> unmappedByTechnicalMissingInCelera\.sam`;
+`samtools view -S unmappedByTechnicalMissingInCelera\.sam | awk \'\{print \$1\}\' \| sort \| uniq \> unmappedByTechnicalMissingInCeleraReadPairs\.lst`;
+`wc \-l unmappedByTechnicalMissingInCeleraReadPairs\.lst \> DoublCask\.log`;
+
+`grep \-Fx \-f nameMy\.lst unmappedByTechnicalMissingInCeleraReadPairs\.lst \> unmappedByTechnicalMissingInCeleraReadPairsAlsoInYchr\.lst`;
+`wc \-l unmappedByTechnicalMissingInCeleraReadPairsAlsoInYchr\.lst \> DoublCask\.log`;
+}
