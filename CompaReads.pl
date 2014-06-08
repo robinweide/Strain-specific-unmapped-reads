1a546387c6753212aa16a37594bcae648405618c
robinweide <robinweide@gmail.com>
2014-06-08 20:03:50 +0200

    compareads

--- CompaReads.pl
+++ CompaReads.pl
@@ -23,11 +23,12 @@ foreach my $QUERY (@QUERY){
         # chdir($values[0]);
       	foreach my $hoi (@QUERY){
       		chomp $hoi;
+            my $Qnam = basename($hoi);
       		my @valus = split(/\t/,$hoi);
         	chomp $valus[0];
         	if ($valus[0] eq $values[0]){next};
-			`\/home\/robin\/bin\/compareads\-2\.0\.2\/compare\_reads \-a $values[0] -b $valus[0] -k 39 -t 2  \&\> $values[0]\_in\_$valus[0].compa\-out`;
-			`\/home\/robin\/bin\/compareads\-2\.0\.2\/extract\_reads $values[0] $values[0]\_in\_$valus[0]\.bv`;
+			`\/home\/robin\/bin\/compareads\-2\.0\.2\/compare\_reads \-a $values[0] -b $valus[0] -k 39 -t 2  \&\> $Qname\_in\_$Qnam\.compa\-out`;
+			`\/home\/robin\/bin\/compareads\-2\.0\.2\/extract\_reads $values[0] $Qname\_in\_$Qnam\.bv`;
 		}
 
         
