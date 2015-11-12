c78416f0155683d9f63c754f19c3ddb982826dff
Robin van der Weide <robinweide@gmail.com>
2015-11-12 13:08:04 +0100

    celera_quant added

---
+++ celera_BN_quant.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use File::Basename;
+open(INFILE, "$ARGV[0]"); #list of samples
+
+my @QUERY = <INFILE>;
+close INFILE;
+
+foreach my $QUERY (@QUERY){
+        chomp $QUERY;
+        my $Qname = basename($QUERY);
+        my @values = split(/\t/,$QUERY);
+        chomp $values[3];
+        print $values[3];
+        #print "cd ".$values[3]."\n";
+        chdir($values[3]);
+
+        `bedtools intersect \-wa \-u \-abam properly\_mapped\_to\_celera\.bam \-b \/data\_fedor12\/robin\/databases\/EVE\/mappingC\/EVE\_mapped\-to\-Celera\_overlapped\.bed \> reads\_celera\_eve\.bam`;
+        `samtools view reads\_celera\_eve\.bam | awk \'\{print \$1\}\' | sort | uniq \> readnames\-eve\_celera\.lst`;
+        `samtools view properly\_mapped\_to\_celera\.bam | awk \'\{print \$1\}\' | sort | uniq \> all\_celera\_reads\.lst`;
+        `wc \-l all\_celera\_reads\.lst \>\> report\_balfour\.stats`;
+        `wc \-l readnames\-eve\_celera\.lst \>\> report\_balfour\.stats`;
+
+        print $values[3]." is done\n";
+        chdir "..";
+}
